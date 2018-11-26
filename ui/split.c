ca32d99889c6aaab7472994400084d90cea2f1b6
Unknown <blackjack4494@web.de>
2018-11-26 08:41:32 +0100

    hotfix, output order, asn

--- ui/split.c
+++ ui/split.c
@@ -103,25 +103,22 @@ void split_redraw(
                 name = str;
             }
             /* May be we should test name's length */
-            snprintf(newLine, sizeof(newLine), "%s %d %d %d %d %d %d %d %d %d %d %d %d %d",
+            snprintf(newLine, sizeof(newLine), "%s %d %d %d %d %d %d %d %d %d %d %d %d %d %s",
                      name, 
 					 net_loss(at), 
 					 net_returned(at), 
 					 net_xmit(at),
-                     net_best(at) / 1000, 
-					 net_avg(at) / 1000,
-                     net_worst(at) / 1000,
-					 /* new values */
-					 //net_err(at),
-					 //net_drop(at),
 					 net_last(at) / 1000,
-					 net_gmean(at) / 1000,
+					 net_avg(at) / 1000,
+                     net_best(at) / 1000,					 
+                     net_worst(at) / 1000,
 					 net_stdev(at) / 1000,
+					 net_gmean(at) / 1000,
 					 net_jitter(at) / 1000,
-					 net_jworst(at) / 1000,
 					 net_javg(at) / 1000,
+					 net_jworst(at) / 1000,
 					 net_jinta(at) / 1000,
-					 //net_up(at)					 
+					 fmt_ipinfo(addr)
 					 );
         } else {
             snprintf(newLine, sizeof(newLine), "???");
